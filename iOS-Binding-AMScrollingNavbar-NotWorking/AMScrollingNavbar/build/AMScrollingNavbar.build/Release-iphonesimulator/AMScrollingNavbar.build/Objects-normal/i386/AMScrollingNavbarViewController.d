5977f3d43c9a7125d71c41158a198c0a5ed83e52
Jonathan Goldberger <github@johnnygold.com>
2014-11-06 16:11:49 -1000

    Why?

--- iOS-Binding-AMScrollingNavbar-NotWorking/AMScrollingNavbar/build/AMScrollingNavbar.build/Release-iphonesimulator/AMScrollingNavbar.build/Objects-normal/i386/AMScrollingNavbarViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/koszachos/Documents/My\ Projects/Xamarin/Xamarin.iOS/Bindings/AMScrollingNavbar/AMScrollingNavbar/AMScrollingNavbar/AMScrollingNavbarViewController.m \
-  /Users/koszachos/Documents/My\ Projects/Xamarin/Xamarin.iOS/Bindings/AMScrollingNavbar/AMScrollingNavbar/AMScrollingNavbar/AMScrollingNavbarViewController.h

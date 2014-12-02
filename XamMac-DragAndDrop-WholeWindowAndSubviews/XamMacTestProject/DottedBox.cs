0c0ba972aa78620e8e5d3356221794f15c3b6d53
Jonathan Goldberger <github@johnnygold.com>
2014-12-01 15:26:32 -1000

    Dashed Box Outline

---
+++ XamMac-DragAndDrop-WholeWindowAndSubviews/XamMacTestProject/DottedBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Foundation;
+using AppKit;
+using CoreGraphics;
+
+namespace XamMacTestProject
+{
+	[Register("DottedBox")]
+    public class DottedBox : NSBox
+    {
+		public DottedBox(IntPtr handle):base(handle)
+		{
+
+		}
+
+		public override void DrawRect(CGRect dirtyRect)
+		{
+			base.DrawRect(dirtyRect);
+
+			const int dashLength = 5;
+			NSColor lineColor = NSColor.Black;
+			lineColor.Set();
+			NSBezierPath path = new NSBezierPath();
+			for (int w = 0; w < dirtyRect.Width; w += dashLength*2) {
+				// Line along bottom
+				path.MoveTo(new CGPoint(w, 0));
+				path.LineTo(new CGPoint(w+dashLength, 0));
+				// Line along top
+				path.MoveTo(new CGPoint(w, dirtyRect.Height));
+				path.LineTo(new CGPoint(w+dashLength, dirtyRect.Height));
+			}
+
+			for (int h = 0; h < dirtyRect.Width; h += dashLength*2) {
+				// Line along left side
+				path.MoveTo(new CGPoint(0, h));
+				path.LineTo(new CGPoint(0, h+dashLength));
+				// Line along right side
+				path.MoveTo(new CGPoint(dirtyRect.Width, h));
+				path.LineTo(new CGPoint(dirtyRect.Width, h+dashLength));
+			}
+			path.Stroke();
+		}
+    }
+}
+

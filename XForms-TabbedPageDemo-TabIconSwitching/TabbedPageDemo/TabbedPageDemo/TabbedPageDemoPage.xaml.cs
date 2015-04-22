9e633e485691dde9887650d2816d2cc99e886050
Jonathan Goldberger <github@johnnygold.com>
2015-04-22 08:30:44 -1000

    XForms-TabbedIconSwitching

---
+++ XForms-TabbedPageDemo-TabIconSwitching/TabbedPageDemo/TabbedPageDemo/TabbedPageDemoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TabbedPageDemo
+{
+	public partial class TabbedPageDemoPage : TabbedPage
+    {
+        public TabbedPageDemoPage()
+        {
+            InitializeComponent();
+
+            this.ItemsSource = MonkeyDataModel.All;
+
+        }
+    }
+}

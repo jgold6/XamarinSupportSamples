176d6e55f30605e1d55217699e515ab064d22b94
Jonathan Goldberger <github@johnnygold.com>
2015-07-13 10:02:54 -1000

    Test Image Perspective with CATransform3D

--- XForms-TestXAMLWebViewTabbed/TestXAMLWebViewTabbed/TestXAMLWebViewTabbed.Android/Resources/Resource.Designer.cs
+++ XForms-TestXAMLWebViewTabbed/TestXAMLWebViewTabbed/TestXAMLWebViewTabbed.Android/Resources/Resource.Designer.cs
@@ -47,7 +47,10 @@ namespace TestXAMLWebViewTabbed.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int Einstein = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
--- XForms-TestXAMLWebViewTabbed/TestXAMLWebViewTabbed/TestXAMLWebViewTabbed.iOS/MyImageRenderer.cs
+++ XForms-TestXAMLWebViewTabbed/TestXAMLWebViewTabbed/TestXAMLWebViewTabbed.iOS/MyImageRenderer.cs
@@ -4,23 +4,22 @@ using Xamarin.Forms.Platform.iOS;
 using UIKit;
 using CoreAnimation;
 
-[assembly: ExportRenderer (typeof (TestXAMLWebViewTabbed.MyLabel), typeof (TestXAMLWebViewTabbed.iOS.MyLabelRenderer))]
+[assembly: ExportRenderer (typeof (Image), typeof (TestXAMLWebViewTabbed.iOS.MyImageRenderer))]
 
 namespace TestXAMLWebViewTabbed.iOS
 {
-    public class MyLabelRenderer : LabelRenderer
+	public class MyImageRenderer : ImageRenderer
     {
-		protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+		protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
 		{
 			base.OnElementChanged(e);
 
 			if (Control != null) {   // perform initial setup
 				// do whatever you want to the UITextField here!
-				Control.TextColor = UIColor.Red;
 
 				CALayer layer = Control.Layer;
 				var transform = CATransform3D.Identity;
-				transform = CATransform3D.MakeRotation ((nfloat)(75 * Math.PI / 180.0f), 0, 1f, 0f);
+				transform = CATransform3D.MakeRotation ((nfloat)(75 * Math.PI / 180.0f), 1f, 0f, 0f);
 				transform.m34 = 1.0f / -500f;
 
 				layer.Transform = transform;

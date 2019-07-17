0e01966626396b30cadf1c61373542856455cd35
Jonathan Goldberger <github@johnnygold.com>
2019-07-17 12:57:48 -1000

    Add new project
    MVVMListAndDetailExample

---
+++ MVVMListAndDetailExample/MVVMListAndDetailExample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace MVVMListAndDetailExample.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        ObservableCollection<PersonViewModel> peopleList = new ObservableCollection<PersonViewModel>();
+        public ObservableCollection<PersonViewModel> PeopleList
+        {
+            get { return peopleList; }
+            set
+            {
+                if (peopleList != value)
+                {
+                    peopleList = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            foreach(Models.Person person in Models.People.Instance.PeopleList)
+            {
+                peopleList.Add(new PersonViewModel(person));
+            }
+        }
+    }
+}
---
+++ MVVMListAndDetailExample/MVVMListAndDetailExample/Views/PersonDetailPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MVVMListAndDetailExample.Views
+{
+    public partial class PersonDetailPage : ContentPage
+    {
+        public PersonDetailPage(ViewModels.PersonViewModel personViewModel)
+        {
+            InitializeComponent();
+            BindingContext = personViewModel;
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
